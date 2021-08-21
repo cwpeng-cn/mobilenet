c1050074efe904bd0924b877f3d7e51dd0443157
“cwpeng-cn” <cw.peng@foxmail.com>
2021-08-21 22:58:43 +0800

    add learning rate scheduler and cuda

--- train.py
+++ train.py
@@ -1,24 +1,31 @@
 from torch.utils.data import DataLoader
-from torch.optim import Adam
+from torch.optim import Adam, lr_scheduler
 from torch.nn import CrossEntropyLoss
 from model import MobileNet
 from dataset import IntelImageClassification
 
 LR = 0.01
+EPOCH = 120
 dataset_path = "../datasets/Intel_image_classification"
 
 net = MobileNet(class_num=6)
 optimizer = Adam(net.parameters(), lr=LR, betas=(0.9, 0.99))
+scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 90, 100], gamma=0.1)
 criterion = CrossEntropyLoss()
 train_dataset = IntelImageClassification(dataset_path=dataset_path, is_train=True)
 test_dataset = IntelImageClassification(dataset_path=dataset_path, is_train=False)
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
-for imgs, labels in train_loader:
-    optimizer.zero_grad()
-    out = net(imgs)
-    loss = criterion(out, labels)
-    loss.backward()
-    optimizer.step()
-    print(loss)
+for epoch in range(EPOCH):
+    scheduler.step()
+    for i, (imgs, labels) in enumerate(train_loader):
+        imgs = imgs.cuda()
+        labels = labels.cuda()
+        optimizer.zero_grad()
+        out = net(imgs)
+        loss = criterion(out, labels)
+        loss.backward()
+        optimizer.step()
+        if i % 5 == 0:
+            print(loss.item())
