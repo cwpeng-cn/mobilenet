9447ea815775c819c52eade73eb6b735df6691eb
“cwpeng-cn” <cw.peng@foxmail.com>
2021-08-22 11:31:48 +0800

    add loss writer

--- dataset.py
+++ dataset.py
@@ -58,3 +58,14 @@ class IntelImageClassification(Dataset):
 
     def __len__(self):
         return len(self.paths)
+
+
+class LossWriter:
+    def __init__(self, save_dir):
+        self.save_dir = save_dir
+
+    def add(self, loss_name, loss, i):
+        with open(os.path.join(self.save_dir, loss_name + ".txt"), mode="a") as f:
+            term = str(i) + " " + str(loss) + "\n"
+            f.write(term)
+            f.close()
--- train.py
+++ train.py
@@ -1,14 +1,16 @@
+import os
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam, lr_scheduler
 from torch.nn import CrossEntropyLoss
 from model import MobileNet
-from dataset import IntelImageClassification
+from dataset import IntelImageClassification, LossWriter
 
 LR = 0.01
 EPOCH = 60
 DATASET_PATH = "../datasets/Intel_image_classification"
-SAVE_PATH = "./MobileNet.pth"
+MODEL_PATH = "./MobileNet.pth"
+LOG_PATH = "./Log.txt"
 
 net = MobileNet(class_num=6).cuda()
 optimizer = Adam(net.parameters(), lr=LR, betas=(0.9, 0.99))
@@ -18,10 +20,12 @@ train_dataset = IntelImageClassification(dataset_path=DATASET_PATH, mode="train"
 val_dataset = IntelImageClassification(dataset_path=DATASET_PATH, mode="val")
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
+writer = LossWriter(save_dir=LOG_PATH)
 best_accuracy = 0
 
 
 def validation():
+    global best_accuracy
     net.eval()
     correct_num = 0
     with torch.no_grad():
@@ -33,12 +37,14 @@ def validation():
         net.train()
     accuracy = correct_num / val_dataset.__len__()
     if accuracy > best_accuracy:
-        torch.save(net.state_dict(), SAVE_PATH)
+        best_accuracy = accuracy
+        torch.save(net.state_dict(), MODEL_PATH)
         print("Current Best Accuracy:{}".format(accuracy))
 
 
+step = 0
 for epoch in range(EPOCH):
-    for i, (imgs, labels) in enumerate(train_loader):
+    for imgs, labels in train_loader:
         imgs = imgs.cuda()
         labels = labels.cuda()
         optimizer.zero_grad()
@@ -46,7 +52,9 @@ for epoch in range(EPOCH):
         loss = criterion(out, labels)
         loss.backward()
         optimizer.step()
-        if i % 5 == 0:
+        if step % 5 == 0:
             print("Epoch:{}, Loss:{}".format(epoch, loss.item()))
+            writer.add(loss_name="MobileNet Loss", loss=loss.item(), i=step)
+        step += 1
     validation()
     scheduler.step()
