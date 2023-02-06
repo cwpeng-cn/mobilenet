0cb64602b9882a91261bf02bab8eb89a9e3c3d60
cwpeng-cn <cw.peng@foxmail.com>
2023-02-06 23:20:51 +0800

    add speed test

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import os
+import time
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam, lr_scheduler
@@ -10,25 +10,25 @@ from utils import LossWriter
 
 LR = 0.01
 EPOCH = 30
+BATCH_SIZE=32
 DATASET_PATH = "../datasets/Intel_image_classification"
 MODEL_PATH = "./MobileNetV1.pth"
 LOG_PATH = "./LogV1.txt"
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device=torch.device("mps") # "dml","cpu"
 net = MobileNet(num_classes=6).to(device)
 optimizer = Adam(net.parameters(), lr=LR, betas=(0.9, 0.99))
 scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[5, 12, 20, 25], gamma=0.1)
 criterion = CrossEntropyLoss()
 train_dataset = IntelImageClassification(dataset_path=DATASET_PATH, mode="train")
 val_dataset = IntelImageClassification(dataset_path=DATASET_PATH, mode="val")
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 writer = LossWriter(save_path=LOG_PATH)
 best_accuracy = 0
 
 print("Model Architecture")
 print(net)
-
 
 def validation():
     global best_accuracy
@@ -44,14 +44,16 @@ def validation():
     accuracy = correct_num / val_dataset.__len__()
     if accuracy > best_accuracy:
         best_accuracy = accuracy
-        torch.save(net.state_dict(), MODEL_PATH, _use_new_zipfile_serialization=False)
+        torch.save(net.state_dict(), MODEL_PATH)
         print("Current Best Accuracy:{}".format(accuracy))
 
 
 step = 0
 length = len(train_loader)
 
+last_time=time.time()
 for epoch in range(EPOCH):
+    start_time=time.time()
     for imgs, labels in train_loader:
         imgs = imgs.to(device)
         labels = labels.to(device)
@@ -63,6 +65,12 @@ for epoch in range(EPOCH):
         if step % 5 == 0:
             print("Epoch:[{}],Step:[{}/{}], Loss:{}".format(epoch, step % length, length, loss.item()))
             writer.add(loss=loss.item(), i=step)
+            print("time:",time.time()-last_time)
+            last_time=time.time()
         step += 1
+    print("\n测试结果:")
+    print("Device:{}, Time: {} sec, Loss:{}".format(device,time.time()-start_time,loss.item()))
     validation()
     scheduler.step()
+    
+   
